c3a7ec0a4e18296fb35a1f33c0d93b762a5220bc
conorprunty <conorprunty@hotmail.com>
2015-02-19 20:46:53 +0000

    tidied up the code

--- js/snapJS.js
+++ js/snapJS.js
@@ -1,69 +1,66 @@
-      var ran1, ran2, score=0, myTime, timer;
-      
-      function begin(){
-		generateNums();
+      var ran1, ran2, score = 0,
+          myTime, timer;
+
+      function begin() {
+          generateNums();
           hideBegin();
           setNum();
           countdownTimer(30000);
           myTime = setInterval(setBackgroundTimer, 550);
-          
-		}
-                          
-                          function setBackgroundTimer(){
-                            generateNums();
-                            randLeft();
-                            randRight();
-                          }
-                          
 
-                          
-                          
-                          function setNum(){
-                            
-                            show();
-                            hideBegin();
-                            randLeft();
-                            randRight();
-                            $('#currentScore').val(score);
-                            
-                          }
-                          
-                          function randLeft(){
-                            
-                            document.getElementById("leftBox").innerHTML = ran1;
-                          }
-                          
-                          function randRight(){
-                            
-                            document.getElementById("rightBox").innerHTML = ran2;
-                          }
+      }
 
-                          
-                          function checkNums(){
-                            
-                            if(ran1==ran2){
-                              
-                              score=score+10;
-                              
-                            }
-                            
-                            else{
-                              score=score-5;
-                            }
-                          }
-                          
-                          function snapFunction(){
-                            
-                            checkNums();
-                            generateNums();
-                            setNum();
-                            
-                          }
-                          
-                          function generateNums(){
-                            
-                            ran1 = parseInt(((Math.random() * 10) + 1));
-                            ran2 = parseInt(((Math.random() * 10) + 1));
-                          }
-                          
-                          
+      function setBackgroundTimer() {
+          generateNums();
+          randLeft();
+          randRight();
+      }
+
+
+
+
+      function setNum() {
+
+          show();
+          hideBegin();
+          randLeft();
+          randRight();
+          $('#currentScore').val(score);
+
+      }
+
+      function randLeft() {
+
+          document.getElementById("leftBox").innerHTML = ran1;
+      }
+
+      function randRight() {
+
+          document.getElementById("rightBox").innerHTML = ran2;
+      }
+
+
+      function checkNums() {
+
+          if (ran1 == ran2) {
+
+              score = score + 10;
+
+          } else {
+              score = score - 5;
+          }
+      }
+
+      function snapFunction() {
+
+          checkNums();
+          generateNums();
+          setNum();
+
+      }
+
+      function generateNums() {
+
+          ran1 = parseInt(((Math.random() * 10) + 1));
+          ran2 = parseInt(((Math.random() * 10) + 1));
+      }
