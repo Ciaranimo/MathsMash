55a356cb69ab986df476f76a47b72689b37bddb5
OldByrne <davidayresbyrne@gmail.com>
2015-03-12 16:02:37 +0000

    cleaned up a bit of css, some in operators and main menu

--- mainmenu.php
+++ mainmenu.php
@@ -1,48 +1,47 @@
-<?php 
+<?php
 
     // connect to DB
-    require("common.php"); 
-     
+    require("common.php");
+
     // Check whether user is logged in
-    if(empty($_SESSION['user'])) 
-    { 
-        // If they are not, redirect to the login page. 
-        header("Location: login.php"); 
-         
-        // this statement is needed 
-        die("Redirecting to login.php"); 
-    } 
-     
-?> 
-Hello <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?> Logged in<br /> 
+    if(empty($_SESSION['user']))
+    {
+        // If they are not, redirect to the login page.
+        header("Location: login.php");
+
+        // this statement is needed
+        die("Redirecting to login.php");
+    }
+
+?>
+Hello <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?> Logged in<br />
 <a href="logout.php">Logout</a>
 <!DOCTYPE html>
 <html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <meta content='width=device-width, initial-scale=1' name='viewport'/>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Maths Mash</title>
-</head>
-<body>
-    <div class="page">
+    <head lang="en">
+        <meta charset="UTF-8">
+        <meta content='width=device-width, initial-scale=1' name='viewport'/>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Maths Mash</title>
+    </head>
+    <body>
+        <div class="page">
 
-        <header>
-		<br/>
-        </header>
-        <div id="content">
-		
-			<div class="gameButton">
-				<div class="gameButtonText">
-					<a href="gamemenu.php">Games</a>
-				</div>
-			</div>
+            <header>
+                <br/>
+            </header>
+            <div id="content">
+
+                    <div class="gameButton">
+                            <a href="gamemenu.php">Games</a>
+                    </div>
+
+            </div>
+
+            <footer>
+
+            </footer>
+
         </div>
-
-        <footer>
-            
-        </footer>
-
-    </div>
-</body>
+    </body>
 </html>
