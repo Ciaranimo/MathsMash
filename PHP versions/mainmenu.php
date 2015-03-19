df09dd43f47ad75f748b972bc93cabb924e24bce
conorprunty <conorprunty@hotmail.com>
2015-03-19 17:45:47 +0000

    test of leaderboard

--- PHP versions/mainmenu.php
+++ PHP versions/mainmenu.php
@@ -1,4 +1,4 @@
-<!--<?php
+<?php
 
     // connect to DB
     require("common.php"); 
@@ -7,10 +7,10 @@
     if(empty($_SESSION['user'])) 
     { 
         // If they are not, redirect to the login page. 
-        header("Location: login.php"); 
+        header("Location: index.php"); 
          
         // this statement is needed 
-        die("Redirecting to login.php"); 
+        die("Redirecting to index.php"); 
     } 
     else
     {
@@ -45,7 +45,7 @@
 ?> 
 
 Hello <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?> Logged in<br /> 
-<a href="logout.php">Logout</a>..>
+<a href="logout.php">Logout</a>-->
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -53,19 +53,7 @@ Hello <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'
     <meta content='width=device-width, initial-scale=1' name='viewport'/>
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Maths Mash</title>
-    <style>
-			ldrbrd{
-			
-			}
-			table, th, td{
-				padding: 5px;
-				border: 1px solid black;
-			}
-			th{
-				text-align: center;
-			
-			}
-		</style>
+
 </head>
 <body>
     <div class="page">
@@ -80,32 +68,34 @@ Hello <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'
 					<a href="gamemenu.php">Games</a>
 				</div>
 			</div>
+			<div class="gameButton">
+				<div class="gameButtonText">
+					<a href="leaderboard.php">Leaderboard</a>
+				</div>
+			</div>
         </div>
         <div id="ldrbrd" align="center">
-        <?php
-        $row = $stmt->fetch(); 
-        if($row)
-        {
-			echo "<table><tr><th>Pos.</th><th>Name</th><th>Score</th></tr>";
-			$count = 1;
-			// output data of first row
-			echo "<tr><td>" . $count. "</td><td>" . $row["username"]. "</td><td> " . $row["score"]. "</td></tr>";
-			// output data of next rows
-			while($row = $stmt->fetch()) {
-				$count++;
-				echo "<tr><td>" . $count. "</td><td>" . $row["username"]. "</td><td> " . $row["score"]. "</td></tr>";
-			}
-			echo "</table>";
-		} else {
-			echo "0 results";
-		}
-        
-        ?>
-        </div>
+			<?php
+				$row = $stmt->fetch(); 
+				if($row)
+				{
+					echo "<table><tr><th>Pos.</th><th>Name</th><th>Score</th></tr>";
+					$count = 1;
+					// output data of first row
+					echo "<tr><td>" . $count. "</td><td>" . $row["username"]. "</td><td> " . $row["score"]. "</td></tr>";
+					// output data of next rows
+					while($row = $stmt->fetch()) {
+						$count++;
+						echo "<tr><td>" . $count. "</td><td>" . $row["username"]. "</td><td> " . $row["score"]. "</td></tr>";
+					}
+					echo "</table>";
+				} else {
+					echo "0 results";
+				}
+			
+			?>
+		</div>
 
-        <footer>
-            
-        </footer>
 
     </div>
 </body>
