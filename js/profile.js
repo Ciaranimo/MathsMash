36bb78668e366c25d7d914abccf0712f279395d6
conorprunty <conorprunty@hotmail.com>
2015-04-02 20:16:47 +0100

    edited size of images

--- js/profile.js
+++ js/profile.js
@@ -7,31 +7,31 @@ function setIcon(){
 		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl1.png'/>";
 	} 	
 	else if(levelNew==2){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl2.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl2.png'/>";
 	}
 	else if(levelNew==3){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl3.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl3.png'/>";
 	}
 	else if(levelNew==4){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl4.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl4.png'/>";
 	}
 	else if(levelNew==5){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl5.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl5.png'/>";
 	}
 	else if(levelNew==6){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl6.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl6.png'/>";
 	}
 	else if(levelNew==7){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl7.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl7.png'/>";
 	}	
 	else if(levelNew==8){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl8.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl8.png'/>";
 	}
 	else if(levelNew==9){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl9.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl9.png'/>";
 	}
 	else if(levelNew==10){
-		document.getElementById("icon").innerHTML="<img src='images/animals/lvl10.png'/>";
+		document.getElementById("icon").innerHTML="<img id='iconSize' src='images/animals/lvl10.png'/>";
 	}
 	document.getElementById('iconSize').height = "150";
 	document.getElementById('iconSize').width = "150";
